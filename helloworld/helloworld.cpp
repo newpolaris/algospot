87f86c5b8252610451ba0ce8db6f3a36e17707b5
newpolaris <newpolaris@gmail.com>
2014-05-03 00:09:09 +0900

    add

---
+++ helloworld/helloworld.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int c;
+	cin >> c;
+	while (c--)
+	{
+		string m;
+		cin >> m;
+		cout << "Hello, " << m << "!" << endl;
+	}
+}
