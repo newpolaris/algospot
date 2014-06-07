c010e8eaad640723a42965dafca4b02135efad6d
newpolaris <newpolaris@gmail.com>
2014-06-07 21:49:30 +0900

    %2x 가 아니라 그냥 %x 해서 하니까 오답뜨넹;

--- uri/uri.cpp
+++ uri/uri.cpp
@@ -1,48 +1,30 @@
 #include <iostream>
 #include <string>
+#pragma warning(disable:4996)
 
 using namespace std;
 
 int main()
 {
-	int n;
+	int n, m, i;
 	cin >> n;
 
 	while (n--)
 	{
 		string str;
 		cin >> str;
-
-		int i = 0;
-		while (str.length() - i > 2)
+		auto ch = str.c_str();
+		i = 0;
+		while (i < str.length())
 		{
-			if (str[i] == '%' && str[i + 1] == '2')
+			if (ch[i] == '%') 
 			{
-				switch (str[i + 2])
-				{
-				case '0': putchar(' '); break;
-				case '1': putchar('!'); break;
-				case '4': putchar('$'); break;
-				case '5': putchar('%'); break;
-				case '8': putchar('('); break;
-				case '9': putchar(')'); break;
-				case 'a': putchar('*'); break;
-				default:
-					putchar(str[i]);
-					putchar(str[i + 1]);
-					putchar(str[i + 2]);
-					break;
-				}
+				sscanf(ch+i+1, "%2x", &m);
+				printf("%c", m);
 				i += 3;
 			}
-			else
-			{
-				putchar(str[i]);
-				i++;
-			}
+			else putchar(str[i++]);
 		}
-		while (i < str.length())
-			putchar(str[i++]);
 		cout << endl;
 	}
 
